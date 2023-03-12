2b57f0f53a9afd8eddc199e78874d924ef57b127
Sebas <jbautistas@unal.edu.co>
2023-03-11 22:00:27 -0500

    Creado primer sensor casi adecuado

--- Proyecto1/SnakeArcade/Coordinacion_perspcion_accion/Agente_no_me_estrello.py
+++ Proyecto1/SnakeArcade/Coordinacion_perspcion_accion/Agente_no_me_estrello.py
@@ -2,16 +2,20 @@ from mss import mss
 import numpy as np
 import cv2
 from PIL import Image
+from time import sleep
+import pyautogui
 
 class Agent():
   def __init__(self):
+    self.apple = [7, 12]
+    self.is_apple = True
+    self.there_is_plan = False
+    self.initial = True
+    self.head = [7, 3]
     pass
     
   def program(self, percepts):
-    for p in percepts:
-      if p == 'BUMP':
-        return 'TURN'
-    return 'FORWARD'
+    print('I thinking...')
   
   def sensor(self):
     #bounding_box = {'top': 200, 'left': 0, 'width': 900, 'height': 800} # Joseph
@@ -26,6 +30,7 @@ class Agent():
 
     matrix = np.zeros(shape=(15,17))
 
+    count_apple = 0
     for i in range(15):
       for j in range(17):
         b,g,r = result[i,j]
@@ -35,14 +40,41 @@ class Agent():
           matrix[i,j] = 0
         else:
           matrix[i,j] = 2
-    return matrix
+          self.apple = [i,j]
+          count_apple += 1
+    
+    # Fragmento que se ejecuta solo una vez al inicio
+    if self.initial == True:
+      self.initial = False
+      self.there_is_plan = True
+      matrix[7][4] = 3
+      self.print_matrix(matrix)
+      self.plan = self.program(matrix)
+    # Fragmento que se ejecuta cuando no se encuentra la manzana
+    elif count_apple == 0:
+      self.is_apple = False
+      self.there_is_plan = False
+      self.head = [self.apple[0], self.apple[1]]
+    # Fragmento que se ejecuta cuando se encuentra la manzana
+    elif self.there_is_plan == False:
+      self.there_is_plan = True
+      matrix[self.head[0]][self.head[1]] = 3
+      self.plan = self.program(matrix)
+      self.print_matrix(matrix)
+
+  def action(self, action):
+    pyautogui.keyDown(action)
+    sleep(0.5)
 
   def print_matrix(self, matrix):
     for i in range(15):
       for j in range(17):
         print(int(matrix[i,j]), end=' ')
       print()
+    
+    print('\nApple: ', self.apple)
 
 
 agent = Agent()
-agent.print_matrix(agent.sensor())
+while True:
+  agent.sensor()
